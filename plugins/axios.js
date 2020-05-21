81a2f687789b11979b0029f701550fb3af549570
PixsaOJ <sabasaba101010@gmail.com>
2020-05-21 12:39:23 +0400

    eslint stuff

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,8 @@
 // GLOBAL HEADERS
-export default function ({$axios}) {
+export default function({ $axios }) {
   $axios.setHeader('AUTHORISATION', 'Username Pass')
-  $axios.onRequest(config => {
+  $axios.onRequest((config) => {
+    // eslint-disable-next-line no-console
     console.log('Making request to ' + config.url) // config.url comes from nuxt.config.js from axios config
   })
 }
